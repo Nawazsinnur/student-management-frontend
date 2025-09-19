5fd717bc46721b92a18b5ddfaeb898a593e0bb5e
nwz <nawazsinnur61@gmail.com>
2025-09-19 19:46:20 +0530

    Update API base URL for deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://student-management-backend-4975.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
